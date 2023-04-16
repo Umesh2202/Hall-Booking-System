6b87c03e5cb4cb8c13f2e3d6c8e964f81bb76fbd
Umesh2202 <umesh2002shir@gmail.com>
2023-04-16 10:16:17 +0530

    💥 fix: Fix sveltejs/kit error

--- src/routes/halls/+page.server.ts
+++ src/routes/halls/+page.server.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 import { redis } from '$lib/server/redis';
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+	runtime: 'edge'
+};
 
 export const load = async () => {
 	let data;
