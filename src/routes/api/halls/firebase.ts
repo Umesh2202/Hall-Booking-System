a15e31f123d68a94deb12940b546e59388319913
Umesh2202 <you@example.com>
2023-06-21 08:32:22 +0530

    fix: Minor changes

--- src/routes/api/halls/firebase.ts
+++ src/routes/api/halls/firebase.ts
@@ -4,14 +4,7 @@ import { getFirestore, collection } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyApS8fjXbRVddqbOA7ccbv1qDdGTkt8zZA',
-	authDomain: 'todo-list-42292.firebaseapp.com',
-	databaseURL: 'https://todo-list-42292-default-rtdb.asia-southeast1.firebasedatabase.app',
-	projectId: 'todo-list-42292',
-	storageBucket: 'todo-list-42292.appspot.com',
-	messagingSenderId: '415826039101',
-	appId: '1:415826039101:web:4f476dd9c19f7c80773554',
-	measurementId: 'G-VB8WWQQ6E2'
+	// your config details go here
 };
 
 initializeApp(firebaseConfig);
